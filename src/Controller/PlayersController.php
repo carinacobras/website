0e29613daf37e646f311c78cca2753361a9175d3
Tyson Ross <tross_github@tysonross.com>
2017-09-15 23:01:23 +1000

    $$$

--- src/Controller/PlayersController.php
+++ src/Controller/PlayersController.php
@@ -39,7 +39,7 @@ class PlayersController extends AppController
     public function view($id = null)
     {
         $player = $this->Players->get($id, [
-            'contain' => ['Users', 'Teams', 'Absences', 'Contacts', 'Ladders', 'Contacts.PhoneNumbers', 'Contacts.PhoneNumbers', 'Contacts.Relationships']
+            'contain' => ['Users', 'Teams', 'Absences', 'Contacts', 'Ladders', 'Contacts.PhoneNumbers', 'Contacts.Relationships']
         ]);
 
         $this->set('player', $player);
