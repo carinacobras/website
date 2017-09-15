5323681dbcdb6e34beb1152cadd926ff32fa0ed6
Tyson Ross <tross_github@tysonross.com>
2017-09-15 22:45:38 +1000

    ###

--- src/Controller/PlayersController.php
+++ src/Controller/PlayersController.php
@@ -39,7 +39,7 @@ class PlayersController extends AppController
     public function view($id = null)
     {
         $player = $this->Players->get($id, [
-            'recursive' => 2,
+            'recursive' => 1,
             'contains' => ['Users', 'Teams', 'Absences', 'Contacts', 'Ladders']
         ]);
 
