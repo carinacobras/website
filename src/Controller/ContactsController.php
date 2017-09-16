c2b22b68182278fc4618670f3759b18079fe1a01
Tyson Ross <tross_github@tysonross.com>
2017-09-17 00:30:55 +1000

    ###

--- src/Controller/ContactsController.php
+++ src/Controller/ContactsController.php
@@ -63,7 +63,7 @@ class ContactsController extends AppController
             }
             $this->Flash->error(__('The contact could not be saved. Please, try again.'));
         }
-        $players = $this->Contacts->Players->find('threaded', ['keyField' => 'user_id', '' => 'player_id', 'limit' => 200]);
+        $players = $this->Contacts->Players->find('list', ['fields', 'limit' => 200]);
         $phoneNumbers = $this->Contacts->PhoneNumbers->find('list', ['limit' => 200]);
         $emails = $this->Contacts->Emails->find('list', ['limit' => 200]);
         $relationships = $this->Contacts->Relationships->find('list', ['limit' => 200]);
