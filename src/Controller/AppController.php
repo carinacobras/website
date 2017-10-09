302b39bc9ef1119f9c7ae61a15bf82aa4448fba0
Tyson Ross <tross_github@tysonross.com>
2017-10-09 22:56:41 +1000

    ###

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -44,6 +44,7 @@ class AppController extends Controller
         $this->loadComponent('RequestHandler');
         $this->loadComponent('Flash');
         $this->loadComponent('Auth', [
+            'authorize' => ['Controller'],
             'loginRedirect' => [
                 'controller' => 'Pages',
                 'action' => 'home'
