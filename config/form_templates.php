7f698d1e7eba84e485c5aed1262e623c438958f6
Tyson Ross <tross_github@tysonross.com>
2018-02-23 00:36:20 +1000

    added checkbox wrapper classes

--- config/form_templates.php
+++ config/form_templates.php
@@ -4,6 +4,7 @@
         'inputContainer' => '<div class="form-group">{{content}}</div>',
         'input' => '<input class="form-control" type="{{type}}" name="{{name}}" {{attrs}} />',
         'checkbox' => '<input type="checkbox" class="form-check-input" name="{{name}}" value="{{value}}"{{attrs}}>',
-        'nestingLabel' => '{{hidden}}{{input}}<label class="form-check-label" {{attrs}}>{{text}}</label>'
+        'checkboxWrapper' => '<div class="form-check">{{label}}</div>',
+        'nestingLabel' => '{{hidden}}<label class="form-check-label" {{attrs}}>{{input}}{{text}}</label>'
     ];
 ?>
