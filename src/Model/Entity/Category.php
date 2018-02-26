1367087a051fd00cb625eef58f600d0d172d9ab6
Tyson Ross <tross_github@tysonross.com>
2018-02-26 13:44:05 +1000

    fixed categories bug

--- src/Model/Entity/Category.php
+++ src/Model/Entity/Category.php
@@ -24,7 +24,7 @@ class Category extends Entity
      * @var array
      */
     protected $_accessible = [
-        'title' => true,
-        'posts' => true
+        '*' => true,
+        'id' => false
     ];
 }
