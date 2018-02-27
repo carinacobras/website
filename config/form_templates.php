07c48cb94cf6dad777c22134111bb3d7a5740aa9
Tyson Ross <tross_github@tysonross.com>
2018-02-27 10:28:23 +1000

    set default styles for cakephp button

--- config/form_templates.php
+++ config/form_templates.php
@@ -3,6 +3,7 @@
         'dateWidget' => '{{day}}{{month}}{{year}}{{hour}}{{minute}}{{second}}{{meridian}}',
         'inputContainer' => '<div class="form-group">{{content}}</div>',
         'input' => '<input class="form-control" type="{{type}}" name="{{name}}" {{attrs}} />',
+        'button' => '<button class="btn btn-primary" {{attrs}}>{{text}}</button>',
         'select' => '<select class="form-control" name="{{name}}"{{attrs}}>{{content}}</select>',
         'textarea' => '<textarea class="form-control" name="{{name}}"{{attrs}}>{{value}}</textarea>',
         'checkbox' => '<input type="checkbox" class="form-check-input" name="{{name}}" value="{{value}}"{{attrs}}>',
