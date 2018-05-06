53d06ff5baf580c3fc931a90c0a1161d4a61365c
Tyson Ross <tross_github@tysonross.com>
2018-05-06 22:26:17 +1000

    ##

--- src/Controller/NewslettersController.php
+++ src/Controller/NewslettersController.php
@@ -2,7 +2,7 @@
 namespace App\Controller;
 
 use App\Controller\AppController;
-require dirname(__DIR__) . '../vendor/autoload.php';
+require dirname(__DIR__) . '/../vendor/autoload.php';
 
 /**
  * Newsletters Controller
