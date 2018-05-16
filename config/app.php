8be94b64c0ee5fa1ece70fdf2c19a37cd6baee9a
Tyson Ross <tross_github@tysonross.com>
2018-05-16 16:48:22 +1000

    #

--- config/app.php
+++ config/app.php
@@ -214,7 +214,7 @@ return [
     'Datasources' => [
         'default' => [
             'className' => 'Cake\Database\Connection',
-            'driver' => 'Cake\Database\Driver\Postgres',
+            'driver' => 'Cake\Database\Driver\Mysql',
             'persistent' => false,
             'host' => 'localhost',
             /**
@@ -259,7 +259,7 @@ return [
          */
         'test' => [
             'className' => 'Cake\Database\Connection',
-            'driver' => 'Cake\Database\Driver\Postgres',
+            'driver' => 'Cake\Database\Driver\Mysql',
             'persistent' => false,
             'host' => 'localhost',
             //'port' => 'non_standard_port_number',
