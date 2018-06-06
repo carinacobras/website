fcdf5167d316ba660cd8840ff0aefcc183e99d29
Tyson Ross <tross_github@tysonross.com>
2018-06-06 11:37:18 +1000

    added live newsletter sending

--- src/Shell/EmailShell.php
+++ src/Shell/EmailShell.php
@@ -10,18 +10,35 @@ class EmailShell extends Shell
     public function main()
     {
         $this->Emails = TableRegistry::get('Emails');
-        $this->Newsltters = TableRegistry::get('Newsletters');
+        $this->Newsletters = TableRegistry::get('Newsletters');
 
-        // $emails = $this->Emails->find('all', array(
-        //     'field' => array('Email.email_address')
-        // ));
+        $emails = $this->Emails->find('all', array(
+            'field' => array('Email.email_address')
+        ));
 
-        // $newsletters = 
+        $newsletters = $this->Newsletters->find('all', array(
+            'conditions' => ['status =' => 1]
+        ));
 
+        foreach ($newsletters as $newsletter) {
+            $data = [
+                'status'    => 2
+            ];
+            $this->Newsletters->patchEntity($newsletter, $data);
+            $this->Newsletters->save($newsletter);
+
+            foreach ($emails as $email) {
+                $emailSender = new Email();
+                $emailSender->profile('default');
+                $from_address = 'registrar@carinacobras.com.au';
+
+                $emailSender->from([$from_address])
+                ->to([$email->email_address])
+                ->subject($newsletter['subject'])
+                ->send([$newsletter['body']]);
+            }
+        }
     
-        // foreach ($emails as $email) {
-        //     $this->out($email->email_address);
-        // }
     }
 }
 
